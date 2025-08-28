fa2021146da7b1c9a6afaac880f4ee50196993fa
Slava Svyatoslav <98528117+Afdaser@users.noreply.github.com>
2025-08-29 00:05:31 +0300

    Усунення українських фрагментів у англомовному описі тегів та оновлення перекладів

--- common/messages/en-US/tag.php
+++ common/messages/en-US/tag.php
@@ -1,12 +1,11 @@
 <?php
 return [
     'Найцікавіші опитування на тему "{tag}"' => 'Opinions {tag} polling',
-    'Опитування на тему "{tag}" були створені {date}.' => 'Polls on the topic "{tag}" were first created on our website on {date}, and since then the collection of opinions {tag} polling has grown to {count} polls in total.',
-    'На цю тему на даний момент є {count} опитувань.' => '',
-    'Найпопулярніше серед них — {title}.' => 'Among them, the most popular one is "{title}" which has collected {votes} votes.',
-    'Воно набрало {votes} голосів.' => '',
+    'Опитування на тему "{tag}" були створені {date}, і відтоді колекція думок {tag} зросла до {count} опитувань.' => 'Polls on the topic "{tag}" were first created on our website on {date}, and since then the collection of opinions {tag} polling has grown to {count} polls in total.',
+    'Найпопулярніше серед них — {title}, який набрав {votes} голосів.' => 'Among them, the most popular one is "{title}" which has collected {votes} votes.',
     'Опитування з найбільшим рейтингом — {title} має рейтинг {rating}.' => 'The poll with the highest rating is "{title}" with a rating of {rating}.',
     'Рейтинг присвоюється користувачами сайту до кожного опитування.' => 'Ratings are assigned by site users to each poll, giving a clear picture of current political polls {tag} and other trending discussions.',
     'Останнє опитування по темі {tag} було додано {date}.' => 'The latest polls {tag} update shows that the most recent poll on this topic was added on {date}. Altogether, this forms a wide {tag} survey database where people share their views and opinions on different topics that matter to them.',
     'невідомо' => 'unknown',
 ];
+
--- common/messages/no-NO/tag.php
+++ common/messages/no-NO/tag.php
@@ -1,12 +1,11 @@
 <?php
 return [
     'Найцікавіші опитування на тему "{tag}"' => 'De mest interessante avstemningene om "{tag}"',
-    'Опитування на тему "{tag}" були створені {date}.' => 'Avstemninger om "{tag}" ble opprettet {date}.',
-    'На цю тему на даний момент є {count} опитувань.' => 'Det finnes for øyeblikket {count} avstemninger om dette emnet.',
-    'Найпопулярніше серед них — {title}.' => 'Den mest populære er {title}.',
-    'Воно набрало {votes} голосів.' => 'Den har fått {votes} stemmer.',
-    'Опитування з найбільшим рейтингом — {title} має рейтинг {rating}.' => 'Avstemningen med høyest vurdering er {title} med vurdering {rating}.',
-    'Рейтинг присвоюється користувачами сайту до кожного опитування.' => 'Vurderingen gis av brukerne til hver avstemning.',
-    'Останнє опитування по темі {tag} було додано {date}.' => 'Den siste avstemningen om {tag} ble lagt til {date}.',
+    'Опитування на тему "{tag}" були створені {date}, і відтоді колекція думок {tag} зросла до {count} опитувань.' => 'Avstemninger om "{tag}" ble først opprettet på nettstedet vårt {date}, og siden den gang har samlingen av meninger om {tag} vokst til {count} avstemninger.',
+    'Найпопулярніше серед них — {title}, який набрав {votes} голосів.' => 'Den mest populære er "{title}", som har samlet {votes} stemmer.',
+    'Опитування з найбільшим рейтингом — {title} має рейтинг {rating}.' => 'Avstemningen med høyest vurdering er "{title}" med vurdering {rating}.',
+    'Рейтинг присвоюється користувачами сайту до кожного опитування.' => 'Vurderinger gis av brukerne til hver avstemning, noe som gir et klart bilde av dagens politiske {tag}-avstemninger og andre aktuelle diskusjoner.',
+    'Останнє опитування по темі {tag} було додано {date}.' => 'Den siste {tag}-avstemningsoppdateringen viser at den nyeste avstemningen om dette emnet ble lagt til {date}. Til sammen danner dette en omfattende {tag}-undersøkelsesdatabase der folk deler sine synspunkter om viktige temaer.',
     'невідомо' => 'ukjent',
 ];
+

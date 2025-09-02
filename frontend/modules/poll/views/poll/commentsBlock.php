27ad9a9ce7e2fefa2d2ae6d53c55a44817f4f4ec
Slava Svyatoslav <98528117+Afdaser@users.noreply.github.com>
2025-09-02 22:20:34 +0300

    Переніс мікророзмітку дати коментарів у тег коментаря

--- frontend/modules/poll/views/poll/commentsBlock.php
+++ frontend/modules/poll/views/poll/commentsBlock.php
@@ -1,6 +1,11 @@
 <div class="middle_comments_list_b">
     <?php foreach ($comments as $comment): ?>
         <div class="comment_item" itemprop="comment" itemtype="https://schema.org/Comment" itemscope>
+            <?php
+            $date = new \DateTime($comment->date_add, new \DateTimeZone('UTC'));
+            $date->setTimezone(new \DateTimeZone('America/New_York'));
+            ?>
+            <meta itemprop="datePublished" content="<?= $date->format('c'); ?>">
             <?php if (isset($isNew)): ?>
                 <?= $this->render('/poll/profile_comment', ['comment' => $comment, 'isNew' => true]); ?>
             <?php else: ?>

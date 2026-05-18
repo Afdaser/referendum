639c672cc114fd706074127e6b00e7c10b2ee0e4
Slava Svyatoslav <98528117+Afdaser@users.noreply.github.com>
2026-05-18 21:48:34 +0300

    Оновлено дизайн хедера та мобільну адаптивність

--- frontend/views/layouts/main/header.php
+++ frontend/views/layouts/main/header.php
@@ -1,7 +1,6 @@
 <?php
 
 use frontend\widgets\WLanguageSelector;
-use frontend\widgets\WSearchForm;
 
 /** @var $this Controller */ ?>
 <!-- ~/frontend/views/layouts/main/header.php -->
@@ -9,37 +8,39 @@ use frontend\widgets\WSearchForm;
 ================================================== -->
 <header class="header">
     <div class="container">
-        <div class="row">
-            <?php if (Yii::$app->request->url == '/') : ?>
-                <div class="logo" style="margin-top: 10px; display: inline-block; text-decoration: none;">
-                    <?php // Оновлюємо alt-текст логотипа для актуальної назви сайту. ?>
-                    <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58"><br>
-                    <span class="sub_text_logo"><?= Yii::t("main", 'кожен голос важливий'); ?></span>
-                </div>
-            <?php else: ?>
-                <a href="/" class="logo">
-                    <?php // Оновлюємо alt-текст логотипа для актуальної назви сайту. ?>
-                    <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58">
-                    <br>
-                    <span class="sub_text_logo">
-                        <?php echo Yii::t("main", 'кожен голос важливий'); ?>
-                    </span>
-                </a>
-            <?php endif; ?>
+        <?php // Головна обгортка хедера: адаптивний лейаут для десктопу і мобільних. ?>
+        <div class="row header_row">
+            <div class="header_brand_block">
+                <?php if (Yii::$app->request->url == '/') : ?>
+                    <div class="logo">
+                        <?php // Залишаємо ті самі розміри логотипа (184x58), як вимагалось. ?>
+                        <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58"><br>
+                        <span class="sub_text_logo"><?= Yii::t("main", 'кожен голос важливий'); ?></span>
+                    </div>
+                <?php else: ?>
+                    <a href="/" class="logo">
+                        <?php // Залишаємо ті самі розміри логотипа (184x58), як вимагалось. ?>
+                        <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58">
+                        <br>
+                        <span class="sub_text_logo">
+                            <?php echo Yii::t("main", 'кожен голос важливий'); ?>
+                        </span>
+                    </a>
+                <?php endif; ?>
+            </div>
 
             <div class="right_header_b">
-                <?= WLanguageSelector::widget(); ?>
-                <br>
-<?php /* * / ?>
-                <div class="search_b" style="border:1px dashed red;"><?= WSearchForm::widget([]); ?></div>
-<?php /* */ ?>
-                <div class="search_b">
-                    <form method="post" action="/site/search" name="HeaderSearch">
-                        <input type="hidden" name="<?= Yii::$app->request->csrfParam; ?>" value="<?= Yii::$app->request->csrfToken; ?>" />
-                        <input class="search_input" type="search" name="SearchForm[text]" value="" placeholder="<?= Yii::t("main", 'Пошук'); ?>...">
-                        <a href="#" class="search_btn" onclick="document.forms['HeaderSearch'].submit()"></a>
-                        <input type="checkbox" name="SearchForm[search_in_title]" checked hidden value="1">
-                    </form>
+                <div class="header_controls">
+                    <?= WLanguageSelector::widget(); ?>
+
+                    <div class="search_b">
+                        <form method="post" action="/site/search" name="HeaderSearch">
+                            <input type="hidden" name="<?= Yii::$app->request->csrfParam; ?>" value="<?= Yii::$app->request->csrfToken; ?>" />
+                            <input class="search_input" type="search" name="SearchForm[text]" value="" placeholder="<?= Yii::t("main", 'Пошук'); ?>...">
+                            <a href="#" class="search_btn" onclick="document.forms['HeaderSearch'].submit()"></a>
+                            <input type="checkbox" name="SearchForm[search_in_title]" checked hidden value="1">
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>

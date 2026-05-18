3c38b9de0f59aa666675fce058dd7e0cc26adbf1
Slava Svyatoslav <98528117+Afdaser@users.noreply.github.com>
2026-05-18 22:45:48 +0300

    Оновлено сучасний вигляд хедера без зміни функціоналу

--- frontend/views/layouts/main/header.php
+++ frontend/views/layouts/main/header.php
@@ -9,9 +9,9 @@ use frontend\widgets\WSearchForm;
 ================================================== -->
 <header class="header">
     <div class="container">
-        <div class="row">
+        <div class="row header_row">
             <?php if (Yii::$app->request->url == '/') : ?>
-                <div class="logo" style="margin-top: 10px; display: inline-block; text-decoration: none;">
+                <div class="logo" aria-label="Referendum social logo">
                     <?php // Оновлюємо alt-текст логотипа для актуальної назви сайту. ?>
                     <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58"><br>
                     <span class="sub_text_logo"><?= Yii::t("main", 'кожен голос важливий'); ?></span>
@@ -29,7 +29,7 @@ use frontend\widgets\WSearchForm;
 
             <div class="right_header_b">
                 <?= WLanguageSelector::widget(); ?>
-                <br>
+                <?php // Прибираємо застарілий перенос рядка й керуємо відступами тільки через CSS. ?>
 <?php /* * / ?>
                 <div class="search_b" style="border:1px dashed red;"><?= WSearchForm::widget([]); ?></div>
 <?php /* */ ?>

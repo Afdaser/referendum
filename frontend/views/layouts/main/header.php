ea687eb56b70f7395deed57bf432e2a10c689d7a
Slava Svyatoslav <98528117+Afdaser@users.noreply.github.com>
2026-05-18 23:14:16 +0300

    Оновлено дизайн хедера для сучасного вигляду та мобільних

--- frontend/views/layouts/main/header.php
+++ frontend/views/layouts/main/header.php
@@ -9,15 +9,17 @@ use frontend\widgets\WSearchForm;
 ================================================== -->
 <header class="header">
     <div class="container">
-        <div class="row">
+        <div class="row header_row">
             <?php if (Yii::$app->request->url == '/') : ?>
-                <div class="logo" style="margin-top: 10px; display: inline-block; text-decoration: none;">
+                <?php // Головна сторінка: залишаємо div для семантичної відповідності поточній логіці. ?>
+                <div class="logo header_logo_wrap" style="margin-top: 10px; display: inline-block; text-decoration: none;">
                     <?php // Оновлюємо alt-текст логотипа для актуальної назви сайту. ?>
                     <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58"><br>
                     <span class="sub_text_logo"><?= Yii::t("main", 'кожен голос важливий'); ?></span>
                 </div>
             <?php else: ?>
-                <a href="/" class="logo">
+                <?php // Внутрішні сторінки: залишаємо посилання на головну без зміни функціоналу. ?>
+                <a href="/" class="logo header_logo_wrap">
                     <?php // Оновлюємо alt-текст логотипа для актуальної назви сайту. ?>
                     <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58">
                     <br>
@@ -27,7 +29,7 @@ use frontend\widgets\WSearchForm;
                 </a>
             <?php endif; ?>
 
-            <div class="right_header_b">
+            <div class="right_header_b header_controls_wrap">
                 <?= WLanguageSelector::widget(); ?>
                 <br>
 <?php /* * / ?>

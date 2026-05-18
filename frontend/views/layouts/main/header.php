6ce73e6815caf160fad246be12b5ec4a617f01b8
Slava Svyatoslav <98528117+Afdaser@users.noreply.github.com>
2026-05-18 22:34:07 +0300

    Оновити дизайн хедера для сучасного та мобільного вигляду

--- frontend/views/layouts/main/header.php
+++ frontend/views/layouts/main/header.php
@@ -3,33 +3,33 @@
 use frontend\widgets\WLanguageSelector;
 use frontend\widgets\WSearchForm;
 
-/** @var $this Controller */ ?>
+/** @var $this Controller */
+$isHomePage = Yii::$app->request->url == '/';
+?>
 <!-- ~/frontend/views/layouts/main/header.php -->
 <!-- Header
 ================================================== -->
 <header class="header">
     <div class="container">
-        <div class="row">
-            <?php if (Yii::$app->request->url == '/') : ?>
-                <div class="logo" style="margin-top: 10px; display: inline-block; text-decoration: none;">
-                    <?php // Оновлюємо alt-текст логотипа для актуальної назви сайту. ?>
-                    <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58"><br>
+        <div class="row header_row">
+            <?php if ($isHomePage) : ?>
+                <?php // На головній лишаємо логотип без посилання, щоби не дублювати переходи на поточну сторінку. ?>
+                <div class="logo" aria-label="referendum.social">
+                    <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58">
                     <span class="sub_text_logo"><?= Yii::t("main", 'кожен голос важливий'); ?></span>
                 </div>
             <?php else: ?>
-                <a href="/" class="logo">
-                    <?php // Оновлюємо alt-текст логотипа для актуальної назви сайту. ?>
+                <?php // На внутрішніх сторінках зберігаємо перехід на головну через логотип. ?>
+                <a href="/" class="logo" aria-label="referendum.social">
                     <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58">
-                    <br>
-                    <span class="sub_text_logo">
-                        <?php echo Yii::t("main", 'кожен голос важливий'); ?>
-                    </span>
+                    <span class="sub_text_logo"><?php echo Yii::t("main", 'кожен голос важливий'); ?></span>
                 </a>
             <?php endif; ?>
 
             <div class="right_header_b">
-                <?= WLanguageSelector::widget(); ?>
-                <br>
+                <div class="header_top_tools">
+                    <?= WLanguageSelector::widget(); ?>
+                </div>
 <?php /* * / ?>
                 <div class="search_b" style="border:1px dashed red;"><?= WSearchForm::widget([]); ?></div>
 <?php /* */ ?>
@@ -37,6 +37,7 @@ use frontend\widgets\WSearchForm;
                     <form method="post" action="/site/search" name="HeaderSearch">
                         <input type="hidden" name="<?= Yii::$app->request->csrfParam; ?>" value="<?= Yii::$app->request->csrfToken; ?>" />
                         <input class="search_input" type="search" name="SearchForm[text]" value="" placeholder="<?= Yii::t("main", 'Пошук'); ?>...">
+                        <?php // Кнопку залишаємо посиланням для сумісності зі старим JS-кліком. ?>
                         <a href="#" class="search_btn" onclick="document.forms['HeaderSearch'].submit()"></a>
                         <input type="checkbox" name="SearchForm[search_in_title]" checked hidden value="1">
                     </form>

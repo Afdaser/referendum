e7e728221474c16295fec4e28928975532d6eac0
Slava Svyatoslav <98528117+Afdaser@users.noreply.github.com>
2026-05-18 22:37:55 +0300

    Оновити вигляд хедера та мобільну адаптацію

--- frontend/views/layouts/main/header.php
+++ frontend/views/layouts/main/header.php
@@ -9,27 +9,25 @@ use frontend\widgets\WSearchForm;
 ================================================== -->
 <header class="header">
     <div class="container">
-        <div class="row">
-            <?php if (Yii::$app->request->url == '/') : ?>
-                <div class="logo" style="margin-top: 10px; display: inline-block; text-decoration: none;">
-                    <?php // Оновлюємо alt-текст логотипа для актуальної назви сайту. ?>
-                    <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58"><br>
-                    <span class="sub_text_logo"><?= Yii::t("main", 'кожен голос важливий'); ?></span>
-                </div>
-            <?php else: ?>
-                <a href="/" class="logo">
-                    <?php // Оновлюємо alt-текст логотипа для актуальної назви сайту. ?>
-                    <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58">
-                    <br>
-                    <span class="sub_text_logo">
-                        <?php echo Yii::t("main", 'кожен голос важливий'); ?>
-                    </span>
-                </a>
-            <?php endif; ?>
+        <div class="row header_row">
+            <div class="header_brand_block">
+                <?php if (Yii::$app->request->url == '/') : ?>
+                    <div class="logo" aria-label="Referendum home brand block">
+                        <?php // Зберігаємо той самий розмір логотипа (184x58), щоб не ламати композицію сторінок. ?>
+                        <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58">
+                        <span class="sub_text_logo"><?= Yii::t("main", 'кожен голос важливий'); ?></span>
+                    </div>
+                <?php else: ?>
+                    <a href="/" class="logo" aria-label="<?= Yii::t("main", 'На головну'); ?>">
+                        <?php // Зберігаємо той самий розмір логотипа (184x58), щоб не ламати композицію сторінок. ?>
+                        <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58">
+                        <span class="sub_text_logo"><?= Yii::t("main", 'кожен голос важливий'); ?></span>
+                    </a>
+                <?php endif; ?>
+            </div>
 
             <div class="right_header_b">
                 <?= WLanguageSelector::widget(); ?>
-                <br>
 <?php /* * / ?>
                 <div class="search_b" style="border:1px dashed red;"><?= WSearchForm::widget([]); ?></div>
 <?php /* */ ?>
@@ -37,6 +35,7 @@ use frontend\widgets\WSearchForm;
                     <form method="post" action="/site/search" name="HeaderSearch">
                         <input type="hidden" name="<?= Yii::$app->request->csrfParam; ?>" value="<?= Yii::$app->request->csrfToken; ?>" />
                         <input class="search_input" type="search" name="SearchForm[text]" value="" placeholder="<?= Yii::t("main", 'Пошук'); ?>...">
+                        <?php // Лишаємо submit через клік на іконку, щоб зберегти поточну поведінку інтерфейсу. ?>
                         <a href="#" class="search_btn" onclick="document.forms['HeaderSearch'].submit()"></a>
                         <input type="checkbox" name="SearchForm[search_in_title]" checked hidden value="1">
                     </form>

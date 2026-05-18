1a3c3c04079e4aac3b36314b606c26daa1597a2e
Slava Svyatoslav <98528117+Afdaser@users.noreply.github.com>
2026-05-18 22:06:49 +0300

    Оновлено дизайн хедера та мобільну адаптацію

--- frontend/views/layouts/main/header.php
+++ frontend/views/layouts/main/header.php
@@ -11,7 +11,7 @@ use frontend\widgets\WSearchForm;
     <div class="container">
         <div class="row">
             <?php if (Yii::$app->request->url == '/') : ?>
-                <div class="logo" style="margin-top: 10px; display: inline-block; text-decoration: none;">
+                <div class="logo">
                     <?php // Оновлюємо alt-текст логотипа для актуальної назви сайту. ?>
                     <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58"><br>
                     <span class="sub_text_logo"><?= Yii::t("main", 'кожен голос важливий'); ?></span>

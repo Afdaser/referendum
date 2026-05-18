636b8b3b3b92a20bddfec1fb2565caed5410453b
Slava Svyatoslav <98528117+Afdaser@users.noreply.github.com>
2026-05-18 23:21:15 +0300

    Оновлено дизайн хедера з адаптивною мобільною версією

--- frontend/views/layouts/main/header.php
+++ frontend/views/layouts/main/header.php
@@ -9,16 +9,16 @@ use frontend\widgets\WSearchForm;
 ================================================== -->
 <header class="header">
     <div class="container">
-        <div class="row">
+        <div class="row header_row">
             <?php if (Yii::$app->request->url == '/') : ?>
-                <div class="logo" style="margin-top: 10px; display: inline-block; text-decoration: none;">
-                    <?php // Оновлюємо alt-текст логотипа для актуальної назви сайту. ?>
+                <div class="logo" style="text-decoration: none;">
+                    <?php // Залишаємо той самий розмір логотипу (184x58), щоб зберегти поточну айдентику. ?>
                     <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58"><br>
                     <span class="sub_text_logo"><?= Yii::t("main", 'кожен голос важливий'); ?></span>
                 </div>
             <?php else: ?>
                 <a href="/" class="logo">
-                    <?php // Оновлюємо alt-текст логотипа для актуальної назви сайту. ?>
+                    <?php // Залишаємо той самий розмір логотипу (184x58), щоб зберегти поточну айдентику. ?>
                     <img src="/img/layout/logo.png" alt="logo of website referendum.social" width="184" height="58">
                     <br>
                     <span class="sub_text_logo">
@@ -28,31 +28,22 @@ use frontend\widgets\WSearchForm;
             <?php endif; ?>
 
             <div class="right_header_b">
-                <?= WLanguageSelector::widget(); ?>
-                <br>
+                <div class="header_controls">
+                    <?= WLanguageSelector::widget(); ?>
 <?php /* * / ?>
-                <div class="search_b" style="border:1px dashed red;"><?= WSearchForm::widget([]); ?></div>
+                    <div class="search_b" style="border:1px dashed red;"><?= WSearchForm::widget([]); ?></div>
 <?php /* */ ?>
-                <div class="search_b">
-                    <form method="post" action="/site/search" name="HeaderSearch">
-                        <input type="hidden" name="<?= Yii::$app->request->csrfParam; ?>" value="<?= Yii::$app->request->csrfToken; ?>" />
-                        <input class="search_input" type="search" name="SearchForm[text]" value="" placeholder="<?= Yii::t("main", 'Пошук'); ?>...">
-                        <a href="#" class="search_btn" onclick="document.forms['HeaderSearch'].submit()"></a>
-                        <input type="checkbox" name="SearchForm[search_in_title]" checked hidden value="1">
-                    </form>
+                    <div class="search_b">
+                        <form method="post" action="/site/search" name="HeaderSearch">
+                            <input type="hidden" name="<?= Yii::$app->request->csrfParam; ?>" value="<?= Yii::$app->request->csrfToken; ?>" />
+                            <input class="search_input" type="search" name="SearchForm[text]" value="" placeholder="<?= Yii::t("main", 'Пошук'); ?>...">
+                            <?php // Кнопку залишаємо anchor-елементом для сумісності з поточними JS-обробниками. ?>
+                            <a href="#" class="search_btn" onclick="document.forms['HeaderSearch'].submit()" aria-label="<?= Yii::t('main', 'Пошук'); ?>"></a>
+                            <input type="checkbox" name="SearchForm[search_in_title]" checked hidden value="1">
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<?php /*
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-GBLWFCYF8N"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-GBLWFCYF8N');
-</script>
-<?php /* */ ?>
 </header>

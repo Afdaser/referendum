9311300c88f732b4949618e63143972b5c8c5eb5
Slava Svyatoslav <98528117+Afdaser@users.noreply.github.com>
2026-06-01 21:53:40 +0300

    Виправити перенос бургер-меню на малих екранах

--- frontend/views/layouts/main/header.php
+++ frontend/views/layouts/main/header.php
@@ -1,7 +1,8 @@
 <?php
 
+use frontend\helpers\Url;
 use frontend\widgets\WLanguageSelector;
-use frontend\widgets\WSearchForm;
+use yii\helpers\Html;
 
 /** @var $this Controller */ ?>
 <!-- ~/frontend/views/layouts/main/header.php -->
@@ -26,18 +27,41 @@ use frontend\widgets\WSearchForm;
             <?php endif; ?>
 
             <div class="right_header_b">
+                <?php // У хедері залишаємо тільки вибір піддомену, а навігацію та пошук переносимо у бургер-меню. ?>
                 <?= WLanguageSelector::widget(); ?>
-<?php /* * / ?>
-                <div class="search_b" style="border:1px dashed red;"><?= WSearchForm::widget([]); ?></div>
-<?php /* */ ?>
-                <div class="search_b">
-                    <form method="post" action="/site/search" name="HeaderSearch">
-                        <input type="hidden" name="<?= Yii::$app->request->csrfParam; ?>" value="<?= Yii::$app->request->csrfToken; ?>" />
-                        <input class="search_input" type="search" name="SearchForm[text]" value="" placeholder="<?= Yii::t("main", 'Пошук'); ?>...">
-                        <a href="#" class="search_btn" onclick="document.forms['HeaderSearch'].submit()"></a>
-                        <input type="checkbox" name="SearchForm[search_in_title]" checked hidden value="1">
-                    </form>
-                </div>
+                <details class="site_burger_menu">
+                    <summary class="site_burger_toggle" aria-label="<?= Html::encode(Yii::t('main', 'Відкрити меню')); ?>">
+                        <span class="site_burger_icon" aria-hidden="true"></span>
+                        <span class="site_burger_text"><?= Html::encode(Yii::t('main', 'Меню')); ?></span>
+                    </summary>
+                    <div class="site_burger_panel" role="navigation" aria-label="<?= Html::encode(Yii::t('main', 'Головне меню')); ?>">
+                        <a class="site_menu_link" href="<?= Html::encode(Url::toRoute(['/news/index'])); ?>">
+                            <?= Html::encode(Yii::t('poll', 'Новини')); ?>
+                        </a>
+                        <?php if (Yii::$app->user->isGuest): ?>
+                            <a class="site_menu_link site_menu_link_accent" href="<?= Html::encode(Url::toRoute(['/site/login'])); ?>">
+                                <?= Html::encode(Yii::t('main', 'Увійти')); ?>
+                            </a>
+                            <?php // Лишаємо наявний modal-flow реєстрації, щоб не ламати поточний сценарій сайту. ?>
+                            <a class="site_menu_link site_menu_link_accent toggle_modal_registrtion" href="#">
+                                <?= Html::encode(Yii::t('main', 'Зареєструватись')); ?>
+                            </a>
+                        <?php endif; ?>
+                        <div class="site_menu_search">
+                            <form method="post" action="<?= Html::encode(Url::toRoute(['/site/search'])); ?>" name="HeaderBurgerSearch">
+                                <input type="hidden" name="<?= Yii::$app->request->csrfParam; ?>" value="<?= Yii::$app->request->csrfToken; ?>" />
+                                <label class="site_menu_search_label" for="header-burger-search-input">
+                                    <?= Html::encode(Yii::t('main', 'Пошук')); ?>
+                                </label>
+                                <div class="search_b site_menu_search_box">
+                                    <input id="header-burger-search-input" class="search_input" type="search" name="SearchForm[text]" value="" placeholder="<?= Html::encode(Yii::t('main', 'Пошук')); ?>...">
+                                    <button class="search_btn" type="submit" aria-label="<?= Html::encode(Yii::t('main', 'Пошук')); ?>"></button>
+                                    <input type="checkbox" name="SearchForm[search_in_title]" checked hidden value="1">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </details>
             </div>
         </div>
     </div>

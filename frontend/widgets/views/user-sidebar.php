da6d662ebc3b78668df8fc1d27b012acd35188e5
Slava Svyatoslav <98528117+Afdaser@users.noreply.github.com>
2026-06-30 22:32:48 +0300

    Залишити мої опитування тільки на новому URL

--- frontend/widgets/views/user-sidebar.php
+++ frontend/widgets/views/user-sidebar.php
@@ -30,12 +30,12 @@ $identity = Yii::$app->user->identity;
     <div class="text_authorized_b">
 <?php /* * // Yii1 remove:
         <?php echo Yii::t("user", 'Мій рейтинг'); ?>: <?php echo User::getUserRating(Yii::app()->user->id);?><br>
-        <?php echo Yii::t("user", 'Мої опитування'); ?>: <a href="<?php echo Yii::app()->createUrl('/site/myPolls/')?>"><?php echo User::getPollsCount(Yii::app()->user->id);?></a><br>
         <?php echo Yii::t("user", 'Нові коментарі'); ?>: <a href="<?php echo Yii::app()->createUrl('/user/newComments/')?>"><?php echo Poll::getNewCommentsCount(Yii::app()->user->id);?></a>
  */
 ?>
         <?php echo Yii::t("user", 'Мій рейтинг'); ?>: <?= $identity ? User::getUserRating($identity->id) : 0;?><br>
-        <?php echo Yii::t("user", 'Мої опитування'); ?>: <a href="<?= Url::toRoute('/poll/site/myPolls/')?>"><?= $identity ? User::getPollsCount($identity->id) : 0;?></a><br>
+        <?php // Нова логіка URL: користувацьке посилання має вести тільки на чистий публічний шлях /my-polls. ?>
+        <?php echo Yii::t("user", 'Мої опитування'); ?>: <a href="/my-polls"><?= $identity ? User::getPollsCount($identity->id) : 0;?></a><br>
         <?php echo Yii::t("user", 'Нові коментарі'); ?>: <a href="<?= Url::toRoute('/user/new/new-comments/')?>"><?= $identity ? Poll::getNewCommentsCount($identity->id) : 0;?></a>
     </div>
     <a href="<?= Url::toRoute('/user/profile');?>" class="my_profile" role="tablist">
@@ -52,15 +52,15 @@ $identity = Yii::$app->user->identity;
  * <?= $this->render('create-poll-modal', ['pollModel' => $pollModel]); ?>
  */ ?>
 <?php
-// Регресійний захист: кнопка "Створити нове" на /site/myPolls має відкривати модалку.
+// Регресійний захист: кнопка "Створити нове" на /my-polls має відкривати модалку.
 // Тому повертаємо рендер create-poll-modal лише на сторінці "Мої опитування",
 // а не глобально на всіх сторінках авторизованого користувача.
 $requestedRoute = Yii::$app->requestedRoute;
 $controllerRoute = Yii::$app->controller ? Yii::$app->controller->route : '';
 $pathInfo = trim(Yii::$app->request->pathInfo, '/');
-$isMyPollsPage = in_array($requestedRoute, ['poll/site/my-polls', 'site/my-polls', 'site/myPolls'], true)
+$isMyPollsPage = in_array($requestedRoute, ['poll/site/my-polls', 'site/my-polls'], true)
     || in_array($controllerRoute, ['poll/site/my-polls', 'site/my-polls'], true)
-    || strpos($pathInfo, 'site/myPolls') === 0
+    || strpos($pathInfo, 'my-polls') === 0
     || strpos($pathInfo, 'poll/site/my-polls') === 0;
 ?>
 <?php if ($isMyPollsPage): ?>

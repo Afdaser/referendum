997de673f6a15a89a4d1a2f17ce6eece699571fc
Slava Svyatoslav <98528117+Afdaser@users.noreply.github.com>
2026-09-23 23:48:56 +0300

    Уточнити призначення перемикача країни

--- frontend/widgets/views/language-selector.php
+++ frontend/widgets/views/language-selector.php
@@ -1,11 +1,11 @@
-<?php // Доступна назва пояснює призначення селектора незалежно від видимого переліку країн. ?>
+<?php // Селектор перемикає країну піддомену; назва не ототожнює країну з мовою інтерфейсу. ?>
 <select
     class="language_select"
-    aria-label="<?= \yii\helpers\Html::encode(Yii::t('main', 'Мова сайту')); ?>"
+    aria-label="<?= \yii\helpers\Html::encode(Yii::t('main', 'Країна піддомену')); ?>"
     onchange='document.location.href = $(this).val()'
 >
     <?php foreach($languages as $language):?>
-        <?php // Показуємо країни у перемикачі мов замість назв мов. ?>
+        <?php // Показуємо назви країн, яким відповідають доступні піддомени. ?>
         <option <?= (Yii::$app->language == $language->locale) ? ' selected="selected"' : ''; ?> value="<?= Yii::$app->urlManager->createLangUrl($language->name, "/"); ?>"><?= \common\models\Language::getCountryTitle($language); ?></option>
     <?php endforeach;?>
 </select>
